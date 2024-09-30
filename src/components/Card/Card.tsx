3f3b45705460882678797d9841ccc4444bc328cc
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-09-30 18:15:24 -0500

    ensayo imagen

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,7 +13,7 @@ const Card = ({product}: ProductProps) => {
     <Link href={`/products/${product.id}`}>
     <div className="p-6 rounded-xl shadow-xl transition-all duration-300 hover:shadow-2xl relative">
       <button className="absolute top-4 right-4 w-10 h-10 bg-[--tertiary-color] rounded-full shadow-lg flex items-center justify-center z-50">
-      <Image src="/info.png" alt="cart Icon" width={25} height={25} className="w-auto h-auto"/>
+      <Image src="/info.png" alt="Info" width={25} height={25} className="w-auto h-auto"/>
     </button>
         <Image src={product.image} alt={product.name} className="rounded-md" width={300} height={300}/>
         <h3 className={styles.title}>{product.name}</h3>
